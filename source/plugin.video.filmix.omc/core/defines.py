33e5899ca34c7bd7baf454d303de12b2bd101bab
Walter <walomc@yandex.ru>
2018-05-17 20:53:58 +0300

    fix domain to filmix.co

--- source/plugin.video.filmix.omc/core/defines.py
+++ source/plugin.video.filmix.omc/core/defines.py
@@ -3,7 +3,8 @@
 #SITE_DOMAIN = 'filmix.me'
 #SITE_DOMAIN = 'filmix.cc'
 #SITE_DOMAIN = 'filmix.info'
-SITE_DOMAIN = 'filmix.nl'
+#SITE_DOMAIN = 'filmix.nl'
+SITE_DOMAIN = 'filmix.co'
 SITE_URL = 'http://'+SITE_DOMAIN
 PLUGIN_ID = 'plugin.video.filmix.omc'
 CACHE_DATABASE = 'filmix.cache.db'
